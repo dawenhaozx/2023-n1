d9ec27f83fbcb8560ff872b6532b9a278bec7d2a
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2022-05-21 14:39:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
